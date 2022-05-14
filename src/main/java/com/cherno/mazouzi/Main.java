5589f32dd0656dc1e1b5fcee8e09cc5f554849e4
Malik <benanesmalek@gmail.com>
2022-05-14 16:08:45 +0200

    first commit

---
+++ src/main/java/com/cherno/mazouzi/Main.java
@@ -0,0 +1,9 @@
+package com.cherno.mazouzi;
+
+public class Main {
+
+    // https://github.com/google/guava
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
---
+++ src/main/java/com/cherno/mazouzi/common/Predicate.java
@@ -0,0 +1,4 @@
+package com.cherno.mazouzi.common;
+
+public class Predicate {
+}
